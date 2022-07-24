cf5ebbc07f6437f134e694c568e6a417349d4768
Ahmed.mohsen <ahmed.mohsen@akqa.com>
2022-07-24 11:30:29 +0200

    The form under "Follow my journey" removed

--- components/Footer.js
+++ components/Footer.js
@@ -66,7 +66,7 @@ export const Footer = ({ withSignUpForm = true, settings }) => {
     <Bounded as="footer">
       <div className="grid grid-cols-1 justify-items-center gap-24">
         <HorizontalDivider />
-        {withSignUpForm && <SignUpForm settings={settings} />}
+        {/* {withSignUpForm && <SignUpForm settings={settings} />} */}
         <div className="mx-auto w-full max-w-3xl text-center text-xs font-semibold tracking-tight primary-content">
           Proudly published using{" "}
           <PrismicLink href="https://prismic.io" className="secondary-content">
